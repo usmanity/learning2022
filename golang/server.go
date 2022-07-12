9a0ab1f40d3e48764ee3b8590403ec569656e0cd
Muhammad Usman <muhammad@squareup.com>
2022-07-12 16:22:24 -0700

    ugh go

---
+++ golang/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func hello(rw http.ResponseWriter, req *http.Request) {
+	rw.Write([]byte("hello\n"))
+}
+
+func postReq(rw http.ResponseWriter, req *http.Request) {
+	type payload struct {
+		User string
+	}
+	pl := payload{}
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&pl)
+	if err != nil {
+		http.Error(rw, "something went wrong", http.StatusBadRequest)
+	}
+	// tell me how Go is simpler than another lang?
+	rw.Write([]byte(pl.User))
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/post", postReq)
+
+	http.ListenAndServe(":8090", nil)
+}
